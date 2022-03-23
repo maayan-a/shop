1d4b64dc8dcc57079fc688822b77c248b1fac396
Maayan <maayan1243@gmail.com>
2022-03-23 18:16:52 +0200

    continue

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,17 +1,14 @@
 import "./Product.css";
 
-function Product() {
+function Product({ imgSrc, name, price }) {
   return (
     <div className="Product-card">
       <div className="Product-image">
-        <img
-          src="https://www.photofamilia.com/wp-content/uploads/2017/09/%D7%97%D7%95%D7%9C%D7%A6%D7%94-%D7%91%D7%9C%D7%95%D7%A0%D7%99%D7%9D-%D7%AA%D7%9E%D7%95%D7%A0%D7%94.jpg"
-          alt="Product img"
-        />
+        <img src={imgSrc} alt={name} />
       </div>
       <div className="Product-info">
-        <h5>shirt</h5>
-        <h5>5$</h5>
+        <h5>{name}</h5>
+        <h5>{price} $</h5>
       </div>
     </div>
   );
